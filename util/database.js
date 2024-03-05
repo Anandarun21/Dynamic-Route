43831bb40d8a2472022cb3492a33cd2eb4525310
Anandarun21 <anandarun238@gmail.com>
2024-03-05 13:46:38 +0530

    integrated mysql

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Arunvenkat@74'
+});
+
+module.exports = pool.promise();
